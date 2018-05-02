d8b10316d211d8e1cde03e70e064b1fd4880e9d9
Dmitry Sergeyev <deepnekro@gmail.com>
2018-05-02 17:16:44 +0300

    treemap

--- src/main/java/SubscribersForm.java
+++ src/main/java/SubscribersForm.java
@@ -30,11 +30,26 @@ public class SubscribersForm extends JFrame {
         abonentTreeMap.put("111111111", new Abonent("Petrov","Petr", "Petrovich","Yanhelia 16"));
         abonentTreeMap.put("222222222", new Abonent("Ivanov","Ivan", "Ivanovoch","Borshyagovskaya 146"));
         abonentTreeMap.put("333333333", new Abonent("Stepanov","Stepan", "Stepanovich","Politechnicheskiy 10"));
-        abonentTreeMap.put("333333333", new Abonent("Andreev","Andrey", "Andreevich","Volzalnaya 1"));
+        abonentTreeMap.put("444444444", new Abonent("Andreev","Andrey", "Andreevich","Volzalnaya 1"));
 
-        List<Pair<String,Abonent>> abonentList = new ArrayList<Pair<String, Abonent>>();
+
+        List<Abonent> abonentList = new ArrayList<Abonent>();
+
+        for (Map.Entry<String,Abonent> entry: abonentTreeMap.entrySet()
+             ) {
+            abonentList.add(entry.getValue());
+           // System.out.println(entry.getValue());
+        }
        // Collections.sort(abonentList,);
         //abonentList.sort(Comparator.comparing(m -> m.get("sent_date"), format)));
+        List<Abonent> sortedList = new ArrayList<>();
+        abonentList.stream().sorted((p1, p2) -> p1.getName().compareTo(p2.getName())).forEach((p)->{
+            sortedList.add(p);
+           // System.out.println(p);
+        });
+       // sortedList.stream().forEach(System.out::println);
+
+
     }
 
 }
