24cfe17f6f028d9c5e33dfc6c60f4a594054288e
Dmitry Sergeyev <deepnekro@gmail.com>
2018-05-02 19:26:06 +0300

    treemap

--- src/main/java/SubscribersForm.java
+++ src/main/java/SubscribersForm.java
@@ -2,8 +2,11 @@ import javafx.util.Pair;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SubscribersForm extends JFrame {
     private JButton searchButton;
@@ -22,10 +25,12 @@ public class SubscribersForm extends JFrame {
         setVisible(true);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+       // searchButton = new JButton("search");
     }
 
     public static void main(String[] args) {
-        new SubscribersForm();
+        SubscribersForm subscribersForm = new SubscribersForm();
         TreeMap<String, Abonent> abonentTreeMap = new TreeMap<String, Abonent>();
         abonentTreeMap.put("111111111", new Abonent("Petrov","Petr", "Petrovich","Yanhelia 16"));
         abonentTreeMap.put("222222222", new Abonent("Ivanov","Ivan", "Ivanovoch","Borshyagovskaya 146"));
@@ -45,11 +50,41 @@ public class SubscribersForm extends JFrame {
         List<Abonent> sortedList = new ArrayList<>();
         abonentList.stream().sorted((p1, p2) -> p1.getName().compareTo(p2.getName())).forEach((p)->{
             sortedList.add(p);
-           // System.out.println(p);
+            System.out.println(p);
         });
        // sortedList.stream().forEach(System.out::println);
+      //  subscribersForm.searchButton = new JButton("search");
 
+        subscribersForm.searchButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+
+                if (subscribersForm.comboBox1.getSelectedItem() == "phone number") {
+                    for (String key : abonentTreeMap.keySet()
+                            ) {
+                        if (key.equals(subscribersForm.textField6.getText())) {
+                            subscribersForm.textField1.setText(key);
+                        }
+
+                    }
+                } else
+                if (subscribersForm.comboBox1.getSelectedItem() == "surname") {
+                    for (Abonent abonent : abonentTreeMap.values()
+                            ) {
+                        if (abonent.getSurname().equals(subscribersForm.textField6.getText())) {
+                            subscribersForm.textField1.setText(abonentTreeMap.entrySet()
+                                    .stream()
+                                    .filter(entry -> Objects.equals(entry.getValue(), abonent))
+                                    .map(Map.Entry::getKey));
+                        }
+
+                    }
+                }
+            }
+        });
 
     }
 
+
 }
+
+
